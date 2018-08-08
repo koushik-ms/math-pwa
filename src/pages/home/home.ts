e89402579ff52dd5e15982c7f01819bb6fed8286
Koushik Meenakshisundaram <koushik.ms@gmail.com>
2018-08-08 18:44:28 +0530

    Views ready and working on browser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  // Some char codes for result
+  // Incorrect: &#x2717(✗); &#x2718(✘); Color: #f53d3d, #ff0000;
+  // Correct: &#x2713(✓); &#x2714(✔); Color: #32db64, #00ff00;
+  result: String = "";
+  resultColor: String = "";
+  multiplicand: number = 11;
+  multiplier: number = 12;
+  response: number = 0;
+  responseStr: String = "???";
   constructor(public navCtrl: NavController) {
-
   }
 
+  resultSuccess() {
+    this.result = "✔";
+    this.resultColor = "#32db64";
+  }
+
+  resultFailure() {
+    this.result = "✘";
+    this.resultColor = "#f53d3d";
+  }
 }
